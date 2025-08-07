dc32b129829b5727db2d46905865cfc315444885
Sehaj <sehaj@example.com>
2025-08-07 18:49:18 +0530

    Fresh start commit

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../utils/upload');
+const upload = require('../middleware/upload');
+
 
 router.post('/image', upload.single('image'), (req, res) => {
   if (!req.file) {
